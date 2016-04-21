d6edd5a994b1dc53e2ede72a6da47d53877a7da8
Dan Hastings <tshakah@gmail.com>
2016-04-21 12:35:52 +0100

    Add countdown, shutdown and reset code
    This adds most of the needed functionality (as well as minor unmentioned
    changes):
    
    - Countdown code, which shows hours remaining (using the LEDs that are
      also used to select hours till shutdown). This isn't very accurate
      yet, and is also set to just a few seconds for testing.
    - Shutdown code. This affects a pin connected to a transistor, which
      allows the generator shutdown timer to run and turn off the engine
      (I'll have to double check the generator manual, but I think the pin
      needs to be held for the whole duration, otherwise the generator
      control panel will reset the shutdown and not stop).
    - Reset code, which resets everything so we can start another timer.

--- src/generator_control.ino
+++ src/generator_control.ino
@@ -8,32 +8,39 @@
   http://www.kennethkuhn.com/electronics/debounce.c (this algorithm worked best)
 */
 
-byte buttons[] = {8, 9}; // Pins the buttons are wired to
+byte buttons[] = {8, 9, 12}; // Pins the buttons are wired to
 byte leds[] = {2, 5, 6, 7}; // Pins the LEDs are wired to
+int transistor = 13;
+int power = 14;
 int currentLED = -1; // This LED and the ones before it are lit up (-1 is none)
+bool countDown;
 
 #define NUMBUTTONS sizeof(buttons)
 #define NUMLEDS sizeof(leds)
-#define MAXIMUM 7
+#define MAXIMUM 7 // Max number of debouncing passes before value changes
 
-byte pressed[NUMBUTTONS], justPressed[NUMBUTTONS];
+byte justPressed[NUMBUTTONS], integrator[NUMBUTTONS], pressed[NUMBUTTONS];
 
 void setup() {
   int i;
+
+  for (i = 0; i < NUMBUTTONS; i++) {
+    pinMode(buttons[i], INPUT_PULLUP);
+  }
 
   for (i = 0; i < NUMLEDS; i++) {
     pinMode(leds[i], OUTPUT);
     digitalWrite(leds[i], LOW);
   }
 
-  for (i = 0; i < NUMBUTTONS; i++) {
-    pinMode(buttons[i], INPUT_PULLUP);
-  }
+  pinMode(transistor, OUTPUT);
+  digitalWrite(transistor, LOW);
+  pinMode(power, OUTPUT);
+  digitalWrite(power, HIGH);
 }
 
 void check_switches() {
   byte input[NUMBUTTONS];
-  static byte integrator[NUMBUTTONS];
 
   for (int i = 0; i < NUMBUTTONS; i++) {
     input[i] = digitalRead(buttons[i]);
@@ -70,6 +77,10 @@ void count_presses() {
     currentLED--;
   }
 
+  if (justPressed[2]) {
+    countDown = true;
+  }
+
   if (currentLED < -1) {
     currentLED = NUMLEDS - 1;
   } else if (currentLED >= NUMLEDS) {
@@ -89,6 +100,76 @@ void change_leds() {
   }
 }
 
+void flash_leds(int timer) {
+  int i;
+
+  for (i = 0; i <= currentLED; i++) {
+    digitalWrite(leds[i], HIGH);
+  }
+
+  sleep(timer / 2);
+
+  for (i = 0; i < NUMLEDS; i++) {
+    digitalWrite(leds[i], LOW);
+  }
+
+  sleep(timer / 2);
+}
+
+void manage_countdown() {
+  while (countDown) {
+    int timer = 2;
+
+    do {
+      flash_leds(1000);
+      timer--;
+    } while(timer > 0);
+
+    currentLED--;
+
+    if (currentLED == -1) {
+      countDown = false;
+      break;
+    }
+  }
+}
+
+void shutdown() {
+  int i;
+  currentLED = NUMLEDS;
+
+  flash_leds(1000);
+  flash_leds(500);
+  flash_leds(500);
+  flash_leds(250);
+  flash_leds(250);
+  flash_leds(250);
+
+  for (i = 0; i < NUMLEDS; i++) {
+    digitalWrite(leds[i], HIGH);
+  }
+
+  digitalWrite(transistor, HIGH);
+
+  sleep(2000);
+}
+
+void reset() {
+  int i;
+  currentLED = -1;
+  digitalWrite(transistor, LOW);
+
+  for (i = 0; i < NUMLEDS; i++) {
+    digitalWrite(leds[i], LOW);
+  }
+
+  for (i = 0; i < NUMBUTTONS; i++) {
+    justPressed[i] = 0;
+    integrator[i] = 0;
+    pressed[i] = 0;
+  }
+}
+
 void loop() {
   int oldCount = currentLED;
 
@@ -98,5 +179,16 @@ void loop() {
 
   if (oldCount != currentLED) {
     change_leds();
+  }
+
+  if (countDown) {
+    for (int i = 0; i < NUMLEDS; i++) {
+      digitalWrite(leds[i], LOW);
+    }
+
+    manage_countdown();
+
+    shutdown();
+    reset();
   }
 }
